5442efa6c82a172fa5cb127b2b97fa8fe4276b40
wesin ribeiro <wesinec@gmail.com>
2023-10-02 09:21:39 -0300

    add insertion sort

---
+++ codigos/07 - Ordenacao/insertionSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    // inicializacao
+    int chave, i;
+    int tamanho = 9;
+    int A[] = {2 ,4 ,1, 10, 3, 5, 4, 12, 8};
+
+    for(int j = 0; j < tamanho; j++){
+        A[j] = rand() % 100;
+        cout << A[j] << "\t";
+    }
+    cout << endl;
+    // processamento
+    for(int j = 2; j < tamanho; j++){
+        chave = A[j];
+        i = j - 1;
+        while(i>=0 && A[i] > chave){
+            A[i + 1] = A[i];
+            i = i -1;
+        }
+        A[i+1] = chave;
+    }
+
+    // finalizacao
+    for(int j = 0; j < tamanho; j++){
+        cout << A[j] << "\t";
+    }
+    cout << endl;
+}
