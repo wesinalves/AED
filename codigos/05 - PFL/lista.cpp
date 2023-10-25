eaf64ae6625ebe4f17c38d450bf28c6fab36956d
wesin ribeiro <wesinec@gmail.com>
2023-10-24 22:41:01 -0300

    add insert method

--- codigos/05 - PFL/lista.cpp
+++ codigos/05 - PFL/lista.cpp
@@ -6,7 +6,6 @@ class Node{
     int data;
     Node* next;
 
-    // Default constructor
     Node(){
         data = 0;
         next = NULL;
@@ -19,15 +18,14 @@ class Node{
 };
 
 class LinkedList{
-
     public:
     Node* head;
-    // default constructor
+
     LinkedList(){
         head = NULL;
     }
 
-    void append(int value){
+    void addNode(int value){
         Node* newNode = new Node(value);
 
         if(head == NULL){
@@ -39,75 +37,48 @@ class LinkedList{
         }
     }
 
+    void insertNode(int pos, int value){
+        Node* prev = NULL;
+        Node* current = head;
+        Node* newNode = new Node(value);
 
-    void deleteItem(int offset){
-        // we need 2 temporary nodes
-        Node *temp1 = head, *temp2 = NULL;
-
-        int len = length();
-
-        // validate the position offset
-        if (len < offset){
-            cout << "Index out of range" << endl;
-            return;
+        for(int i = 0; i < pos; i++){
+            prev = current;
+            current = current->next;
+            if(current == NULL and i < pos){
+                cout << "Index out of range" << endl;
+                return;
+            }
         }
 
-        // delete head if the case
-        if (offset == 0){
-            head = head->next;
+        newNode->next = current;
+        prev->next = newNode;\
 
-            // delete old head
-            delete temp1;
-            return;
+        return;
+
+
+    }
+
+    Node* search(int value){
+        Node* temp = head;
+        while(temp != NULL && temp->data != value){
+            temp = temp->next;
         }
-        
-        // traverse the list until the position
-        while(offset-- > 0){
+        return temp;
+    }
+
+    void deleteNode(int value){
+        Node* temp1 = head;
+        Node* temp2 = NULL;
+
+        while(temp1 != NULL && temp1->data != value){
             temp2 = temp1;
             temp1 = temp1->next;
         }
 
-        // temp2 is the previous position
         temp2->next = temp1->next;
 
         delete temp1;
-
-    }
-
-    void insert(int value, int offset){
-        // we need 2 temporary nodes
-        Node *temp1 = head, *temp2 = NULL;
-        Node* newNode = new Node(value);
-
-        int len = length();
-
-        // validate the position offset
-        if (len < offset){
-            cout << "Index out of range" << endl;
-            return;
-        }
-        
-        while(--offset > -1){
-            temp2 = temp1;
-            temp1 = temp1->next;
-        }
-
-        // temp2 is the previous position
-        newNode->next = temp1;
-        temp2->next = newNode;
-
-    }
-
-    int length(){
-        Node* temp = head;
-        int listLength = -1;
-
-        while(temp != NULL){
-            temp = temp->next;
-            listLength++;
-        }
-
-        return listLength;
     }
 
     void printList(){
@@ -115,33 +86,35 @@ class LinkedList{
             cout << "List is empty" << endl;
             return;
         }
+
         Node* temp = head;
         while(temp != NULL){
             cout << temp->data << "\t";
             temp = temp->next;
         }
+
         cout << endl;
+
     }
+
 };
 
 int main(){
     LinkedList list;
+    Node* x;
 
-    list.append(10);
-    list.append(20);
-    list.append(30);
-    list.append(40);
-    list.append(50);
-    
-    cout << "head of list is " << list.head->data << endl;
+    list.addNode(10);
+    list.addNode(20);
+    list.addNode(30);
+    list.addNode(40);
+    list.addNode(50);
 
     list.printList();
 
-    list.deleteItem(0);
-
-    list.insert(35, 2);
+    list.insertNode(3, 40);
 
     list.printList();
 
     return 0;
+
 }
