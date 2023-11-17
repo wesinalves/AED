76691f412bcae477d14f083f9083db2eee580c7b
wesin ribeiro <wesinec@gmail.com>
2023-11-17 14:59:08 -0300

    add hash table code

--- codigos/09 - Hash/hashTable.cpp
+++ codigos/09 - Hash/hashTable.cpp
@@ -9,7 +9,7 @@ class Hash
 
 	// Pointer to an array containing buckets
 	list<int> *table;
-public:
+	public:
 	Hash(int V); // Constructor
 
 	// inserts a key into hash table
@@ -40,50 +40,50 @@ void Hash::insertItem(int key)
 
 void Hash::deleteItem(int key)
 {
-// get the hash index of key
-int index = hashFunction(key);
+	// get the hash index of key
+	int index = hashFunction(key);
 
-// find the key in (index)th list
-list <int> :: iterator i;
-for (i = table[index].begin();
-		i != table[index].end(); i++) {
-	if (*i == key)
-	break;
-}
+	// find the key in (index)th list
+	list <int> :: iterator i;
+	for (i = table[index].begin();
+			i != table[index].end(); i++) {
+		if (*i == key)
+		break;
+	}
 
-// if key is found in hash table, remove it
-if (i != table[index].end())
-	table[index].erase(i);
+	// if key is found in hash table, remove it
+	if (i != table[index].end())
+		table[index].erase(i);
 }
 
 // function to display hash table
 void Hash::displayHash() {
-for (int i = 0; i < BUCKET; i++) {
-	cout << i;
-	for (auto x : table[i])
-	cout << " --> " << x;
-	cout << endl;
-}
+	for (int i = 0; i < BUCKET; i++) {
+		cout << i;
+		for (auto x : table[i])
+		cout << " --> " << x;
+		cout << endl;
+	}
 }
 
 // Driver program 
 int main()
 {
-// array that contains keys to be mapped
-int a[] = {15, 11, 27, 8, 12};
-int n = sizeof(a)/sizeof(a[0]);
+	// array that contains keys to be mapped
+	int a[] = {15, 11, 27, 8, 12};
+	int n = sizeof(a)/sizeof(a[0]);
 
-// insert the keys into the hash table
-Hash h(7); // 7 is count of buckets in
-			// hash table
-for (int i = 0; i < n; i++) 
-	h.insertItem(a[i]); 
+	// insert the keys into the hash table
+	Hash h(7); // 7 is count of buckets in
+				// hash table
+	for (int i = 0; i < n; i++) 
+		h.insertItem(a[i]); 
 
-// delete 12 from hash table
-h.deleteItem(12);
+	// delete 12 from hash table
+	h.deleteItem(12);
 
-// display the Hash table
-h.displayHash();
+	// display the Hash table
+	h.displayHash();
 
-return 0;
+	return 0;
 }
